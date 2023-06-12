dff09a3c2f6c1423f08a2797032a172904e16a0a
micheg <michelangelog@gmail.com>
2023-06-12 16:08:07 +0000

    fix

--- src/main.js
+++ src/main.js
@@ -1,44 +1,7 @@
 import './css/style.css'
 import config from './conf/config';
-import { Scene, Game } from 'phaser';
-
-class GameScene extends Scene
-{
-
-    constructor()
-    {
-        super('scene-game');
-    }
-
-    create()
-    {
-        let { width, height } = this.sys.game.canvas;
-        console.log(width);
-        this.textbox = this.add.text
-        (
-            width / 2,
-            height / 2,
-            'Welcome to Phaser 3.6!',
-            {
-                color: '#FFF',
-                fontFamily: 'monospace',
-                fontSize: '26px'
-            }
-        );
-
-        this.textbox.setOrigin(0.5, 0.5);
-    }
-
-    update(time, delta)
-    {
-        if (!this.textbox)
-        {
-            return;
-        }
-
-        this.textbox.rotation += 0.0005 * delta;
-    }
-}
+import { Game } from 'phaser';
+import GameScene from './js/scenes/game_scene';
 
 config.scene.push(GameScene);
 
