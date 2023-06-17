9625077d90be78c534a5d30a5c2548907963e5f6
micheg <michelangelog@gmail.com>
2023-06-17 15:25:13 +0200

    added https

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig(
 {
+    plugins: [
+        basicSsl()
+    ],
     build:
     {
         outDir: 'www',
